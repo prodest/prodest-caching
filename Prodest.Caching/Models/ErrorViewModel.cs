da6afbc5f1ba96c3a0ad97e3de10ca7b0c1f191f
Roberto Marconi <robertomarconi@gmail.com>
2021-04-14 17:08:40 -0300

    remove antigos

--- Prodest.Caching/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prodest.Caching.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
